d204d517d402aea06d49c7dc9c52552c2d26b236
Ginajunian <gina10222200@sttcipasung.ac.id>
2025-01-10 20:15:45 +0700

    mengisi halaman profil

--- resources/views/profil/index.blade.php
+++ resources/views/profil/index.blade.php
@@ -3,8 +3,88 @@
         Profil
     </x-slot>
 
-    <div>
-        <h2>haha</h2>
-    </div>
+    <body class="bg-white-100 font-sans">
+    <header class="text-center py-8 mb-6">
+        <img src="img/logo-sttc.png" alt="Logo STT Cipasung" class="w-30 mx-auto mb-4">
+        <h1 class="text-4xl font-bold text-green-600">VISI & MISI</h1>
+    </header>
+    <main class="max-w-7xl mx-auto mt-8 p-8 bg-white shadow-lg rounded-lg">
+        <section class="mb-8">
+            <h2 class="text-2xl font-semibold text-green-600 border-b-2 border-green-600 pb-2 mb-4">Visi STT Cipasung</h2>
+            <p class="text-gray-700">Menjadi Sekolah Tinggi lima besar di Priangan Timur pada tahun 2024 dan unggul di bidang entrepreneurship yang rahmatan lil'alamin.</p>
+        </section>
+        <section class="mb-8">
+            <h2 class="text-2xl font-semibold text-green-600 border-b-2 border-green-600 pb-2 mb-4">Misi STT Cipasung</h2>
+            <ul class="list-disc list-inside text-gray-700 space-y-2">
+                <li>Meningkatkan kualitas pendidikan dan pengajaran yang adil dan akses yang luas</li>
+                <li>Meningkatkan iklim penelitian yang relevan dengan potensi lokal</li>
+                <li>Meningkatkan kegiatan pengabdian kepada masyarakat sesuai dengan kebutuhan dan pembangunan daerah</li>
+                <li>Menyiapkan civitas STT Cipasung agar menjadi pribadi yang berjiwa entrepreneur, berakhlak mulia dan berdaya saing tinggi</li>
+                <li>Memperkuat kerjasama dengan institusi lain di tingkat wilayah maupun di tingkat nasional</li>
+                <li>Meningkatkan tata kelola perguruan tinggi yang transparan dan terukur</li>
+            </ul>
+        </section>
+        <section>
+            <h2 class="text-2xl font-semibold text-green-600 border-b-2 border-green-600 pb-2 mb-4">Tujuan STT Cipasung</h2>
+            <ul class="list-disc list-inside text-gray-700 space-y-2">
+                <li>Menghasilkan lulusan yang berkualitas dan siap berkontribusi di lingkungan masyarakat</li>
+                <li>Menghasilkan riset dan karya ilmiah di bidang teknik dan entrepreneurship yang mendorong pengembangan potensi lokal</li>
+                <li>Menghasilkan kegiatan pengabdian kepada masyarakat yang sesuai dengan kebutuhan dan pembangunan daerah</li>
+                <li>Menjadikan civitas STT Cipasung berkualitas dan siap berkontribusi di lingkungan masyarakat</li>
+                <li>Terealisasinya kerjasama pengembangan institusional, pendidikan dan penelitian baik pada tingkat wilayah maupun nasional</li>
+                <li>Menciptakan tata kelola perguruan tinggi yang transparan dan terukur</li>
+            </ul>
+        </section>
+        <section class="max-w-6xl mx-auto mt-8 p-8 bg-white shadow-lg rounded-lg">
+        <h2 class="text-2xl font-semibold text-green-600 border-b-2 border-green-600 pb-2 mb-4">Tentang STT Cipasung</h2>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <p class="text-gray-700">
+            <strong>Sekolah Tinggi Teknologi Cipasung
+            <br>
+            Didirikan atas prakarsa Drs. KH. Abdul Chobir, MT. dibawah Yayasan Pesantren Cipasung, Tasikmalaya.
+            <br><br></strong>
+                Sekolah Tinggi Teknologi Cipasung (STT Cipasung) merupakan lembaga pendidikan tinggi formal di bawah Yayasan Pondok Pesantren Cipasung. Pesantren Cipasung didirikan tahun 1931 oleh almarhum KH. Ruhiat, terletak di Desa Cipakat Kecamatan Singaparna Kabupaten Tasikmalaya, sekitar 100 km dari Kota Bandung.
+                <br><br>
+                Dalam perkembangan selanjutnya setelah KH. Ruhiat wafat (1931-1977), perjuangan beliau diteruskan oleh KH. Moh. Ilyas Ruhiat (1977-2007), yang pernah menjabat sebagai Rais Aam PBNU (1994-1999), KH Dudung Abdul Halim, MA. (2007-2012), dan sekarang dilanjutkan oleh KH. Drs. A. Bunyamin Ruhiat, M.S.I.
+                <br><br>
+                <strong>STT Cipasung didirikan pada tahun 1997</strong>, melengkapi lembaga-lembaga pendidikan yang sudah ada di lingkungan Pondok Pesantren Cipasung, mulai dari Paud, TK, MI, SMPI, MTs, SMAI, MAN, dan IAIC.
+                <br><br>
+            </p>
+            <p class="text-gray-700">
+                
+                Pemrakarsa utama berdirinya Sekolah Tinggi Teknologi Cipasung adalah <strong>Drs. H. Abdul Chobir, MT</strong> atas anjuran Pimpinan Pondok Pesantren Cipasung pada masa itu, yaitu KH. Moh. Ilyas Ruhiat. Pendirian kampus ini juga atas bantuan dan arahan dari tim Institut Teknologi Bandung, serta dorongan dari Prof. Dr. Ginanjar Kartasasmita dan Ir. Fadel Muhammad ketika melakukan kunjungan ke Pondok Pesantren Cipasung.
+                <br><br>
+                <strong class="text-1xl font-semibold text-green-600 border-b-2 border-green-600 pb-2 mb-4">Sejarah Singkat STT Cipasung</strong>
+                <br><br>
+                <strong>Pendirian STT Cipasung</strong> (25 Mei 1997)
+                Sekolah Tinggi Teknologi Cipasung mendapatkan ijin resmi dari Menteri Pendidikan dan Kebudayaan, berdasarkan surat keputusan no : 35/D/O/1998 tanggal 25 Mei 1998, tentang pemberian status terdaftar kepada dua program studi untuk pendidikan S1 di lingkungan Sekolah Tinggi Teknologi Cipasung, yaitu Program Studi Teknik Industri dan Program Studi Teknik Lingkungan.
+                <br><br>
+                <strong>Prodi Teknik Industri Terakreditasi </strong>(31 Januari 2013)
+                Program Studi Teknik Industri mendapatkan akreditasi dari BAN-PT berdasarkan keputusan no : 030/SK/BAN-PT/Aka-XV/S/I/2013. Berlaku selama lima tahun, sejak tanggal 31 Januari 2013 sampai dengan 31 Januari 2018.
+            </p>
+        </div>
+    </section>
+    <section class="max-w-7xl mx-auto mt-8 p-8 bg-white rounded-lg">
+
+        <h2 class="text-2xl font-semibold text-green-600 border-b-2 border-green-600 pb-2 mb-4">Filosofi Lambang STT Cipasung</h2>
+        <div class="flex flex-col md:flex-row items-center gap-6">
+            <div class="flex-1">
+                <p class="text-gray-700">
+                    <strong>Bentuk segitiga</strong> digali dari akar budaya lokal dari kata "cipasung" yang berarti masung (kerucut – Bahasa Indonesia). Dalam 2 dimensi, bentuk kerucut tampak seperti segitiga. Segitiga melambangkan menyatunya iman, islam, dan ihsan dalam satu kesatuan diri manusia. Segitiga juga melambangkan tiga unsur penting dalam diri manusia yang harus dikerahkan secara optimal dalam kerangka pengabdian seorang hamba, yaitu: rasio, rasa, dan karsa.
+                </p>
+                <p class="text-gray-700 mt-4">
+                    <strong>Warna dasar hijau</strong> secara umum melambangkan simbol Islam dalam budaya Indonesia sekaligus juga melambangkan kesuburan dan kemakmuran.
+                </p>
+                <p class="text-gray-700 mt-4">
+                    <strong>Lima lengkungan dalam segitiga</strong> melambangkan lengkungan bangunan masjid tempat bersujud seorang hamba kepada Allah SWT. Lengkungan-lengkungan itu semakin mengecil dan menggambarkan menuju titik yang satu, yaitu Allah SWT. Jumlah lengkungan yang lima menggambarkan fondasi rukun Islam yang lima sebagai kerangka Sekolah Tinggi Islam, dan lima dasar negara Pancasila sebagai fondasi Negara Kesatuan Republik Indonesia.
+                </p>
+            </div>
+            <div class="w-full md:w-1/3 flex-shrink-0">
+                <img src="img/logo-sttc.png" alt="Lambang STT Cipasung" class="w-full md:w-auto mx-auto">
+            </div>
+        </div>
+    </section>
+    </main>
+</body>
 
 </x-layout>
